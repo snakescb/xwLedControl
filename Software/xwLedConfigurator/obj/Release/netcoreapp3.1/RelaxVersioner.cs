d8e3323b2ae824a0ebe843bb3ff416f61d16e1bc
Christian Luethi <luethich80@gmail.com>
2022-02-26 00:18:25 +0100

    Release

--- Software/xwLedConfigurator/obj/Release/netcoreapp3.1/RelaxVersioner.cs
+++ Software/xwLedConfigurator/obj/Release/netcoreapp3.1/RelaxVersioner.cs
@@ -1,19 +1,19 @@
 // This is auto-generated version information attributes by RelaxVersioner.2.5.0.0, Do not edit.
-// Generated date: Fri, 25 Feb 2022 23:16:07 GMT
+// Generated date: Fri, 25 Feb 2022 23:17:47 GMT
 
 using System.Reflection;
 
-[assembly: AssemblyVersion(@"0.0.37")]
-[assembly: AssemblyFileVersion(@"2022.2.26.227")]
-[assembly: AssemblyInformationalVersion(@"0.0.37-2ef94235f588c791266ca369af9779108057f677")]
-[assembly: AssemblyMetadata(@"Date",@"Fri, 25 Feb 2022 23:07:34 GMT")]
+[assembly: AssemblyVersion(@"0.0.39")]
+[assembly: AssemblyFileVersion(@"2022.2.26.526")]
+[assembly: AssemblyInformationalVersion(@"0.0.39-691cc9864b517729642c531e7783e920f0c20da1")]
+[assembly: AssemblyMetadata(@"Date",@"Fri, 25 Feb 2022 23:17:32 GMT")]
 [assembly: AssemblyMetadata(@"Branch",@"main")]
 [assembly: AssemblyMetadata(@"Tags",@"")]
 [assembly: AssemblyMetadata(@"Author",@"Christian Luethi <luethich80@gmail.com>")]
-[assembly: AssemblyMetadata(@"Committer",@"GitHub <noreply@github.com>")]
-[assembly: AssemblyMetadata(@"Message",@"Merge pull request #4 from snakescb/develop")]
+[assembly: AssemblyMetadata(@"Committer",@"Christian Luethi <luethich80@gmail.com>")]
+[assembly: AssemblyMetadata(@"Message",@"Merge branch 'main' of https://github.com/snakescb/xwLedControl")]
 [assembly: AssemblyMetadata(@"Build",@"")]
-[assembly: AssemblyMetadata(@"Generated",@"Fri, 25 Feb 2022 23:16:07 GMT")]
+[assembly: AssemblyMetadata(@"Generated",@"Fri, 25 Feb 2022 23:17:47 GMT")]
 [assembly: AssemblyMetadata(@"TargetFramework",@"netcoreapp3.1")]
 [assembly: AssemblyMetadata(@"Platform",@"AnyCPU")]
 [assembly: AssemblyMetadata(@"BuildOn",@"Windows_NT")]
@@ -23,19 +23,19 @@ namespace xwLedConfigurator
 {
     internal static class ThisAssembly
     {
-        public const string @AssemblyVersion = @"0.0.37";
-        public const string @AssemblyFileVersion = @"2022.2.26.227";
-        public const string @AssemblyInformationalVersion = @"0.0.37-2ef94235f588c791266ca369af9779108057f677";
+        public const string @AssemblyVersion = @"0.0.39";
+        public const string @AssemblyFileVersion = @"2022.2.26.526";
+        public const string @AssemblyInformationalVersion = @"0.0.39-691cc9864b517729642c531e7783e920f0c20da1";
         public static class AssemblyMetadata
         {
-            public const string @Date = @"Fri, 25 Feb 2022 23:07:34 GMT";
+            public const string @Date = @"Fri, 25 Feb 2022 23:17:32 GMT";
             public const string @Branch = @"main";
             public const string @Tags = @"";
             public const string @Author = @"Christian Luethi <luethich80@gmail.com>";
-            public const string @Committer = @"GitHub <noreply@github.com>";
-            public const string @Message = @"Merge pull request #4 from snakescb/develop";
+            public const string @Committer = @"Christian Luethi <luethich80@gmail.com>";
+            public const string @Message = @"Merge branch 'main' of https://github.com/snakescb/xwLedControl";
             public const string @Build = @"";
-            public const string @Generated = @"Fri, 25 Feb 2022 23:16:07 GMT";
+            public const string @Generated = @"Fri, 25 Feb 2022 23:17:47 GMT";
             public const string @TargetFramework = @"netcoreapp3.1";
             public const string @Platform = @"AnyCPU";
             public const string @BuildOn = @"Windows_NT";
